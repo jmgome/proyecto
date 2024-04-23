8f2b7745a947ef8316b669ab9b888fcace294913
junior gomez <junior.gomez@itconsultores.com.co>
2024-04-23 12:00:54 -0500

    [FEATURE]se creo una funcion con validaciones para una contraseña

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // Función de validación de contraseña
+  function validarContraseña(contraseña) {
+    const longitudMinima = contraseña.length >= 8;
+    const tieneMinuscula = /[a-z]/.test(contraseña);
+    const tieneMayuscula = /[A-Z]/.test(contraseña);
+    const tieneNumero = /\d/.test(contraseña);
+    const tieneCaracterEspecial = /[!@#$%^&*(),.?":{}|<>]/.test(contraseña);
+
+    return (
+      longitudMinima &&
+      tieneMinuscula &&
+      tieneMayuscula &&
+      tieneNumero &&
+      tieneCaracterEspecial
+    );
+  }
+
+  // Función para mostrar el resultado de la validación
+  function validarYMostrar() {
+    const inputContraseña = document.getElementById("contraseña");
+    const resultadoValidacion = document.getElementById("resultadoValidacion");
+
+    if (validarContraseña(inputContraseña.value)) {
+      resultadoValidacion.textContent = "La contraseña es válida.";
+      resultadoValidacion.style.color = "green";
+    } else {
+      resultadoValidacion.textContent =
+        "La contraseña no cumple con los requisitos.";
+      resultadoValidacion.style.color = "red";
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Validación de Contraseña</h2>
+        <label for="contraseña">Introduce tu contraseña:</label>
+        <input type="password" id="contraseña" onblur="validarYMostrar()" />
+        <p id="resultadoValidacion" />
+        <button onClick={validarYMostrar}>Validar</button>
       </header>
     </div>
   );
